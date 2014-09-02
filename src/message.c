98ea97275328437d16beeef497783c28bfdab46a
Arthur LAMBERT <lambertarthur22@gmail.com>
2014-09-02 20:48:42 +0200

    add functon to get token from t411 api

--- src/message.c
+++ src/message.c
@@ -24,60 +24,62 @@ WriteMemoryCallback(void *ptr, size_t size, size_t nmemb, void *data)
   return realsize;
 }
 
-/*
-int t411_auth (const char* user, const char* password)
+char* process_message (CURL *curl, char* url, char* message)
 {
-
-}
-*/
-
-int process_message (char* url, char** arguments)
-{
-  CURL *curl;
   CURLcode res;
+  char buf_error[512];
   struct MemoryStruct chunk;
-  char* message =  malloc (sizeof(char) * (strlen(url) + strlen(T411_URL) + 1));
-
-  arguments = arguments;
+  char* full_url =  malloc (sizeof(char) * (strlen(url) + strlen(T411_URL) + 1));
+  sprintf (full_url, T411_URL, url);
 
   chunk.memory = NULL; /* we expect realloc(NULL, size) to work */
   chunk.size = 0;      /* no data at this point */
 
-  curl_global_init(CURL_GLOBAL_ALL);
+  if (curl == NULL)
+    curl = curl_easy_init();
 
-  curl = curl_easy_init();
-
-  sprintf (message, T411_URL, url);
-  PRINT_DEBUG ("[DEBUG] message : |%s|\n", message);
-  PRINT_DEBUG ("[DEBUG] arguments : |%s|\n", "username=Evanok&password=evangelius");
-
-  if(curl)
+  if (!curl)
   {
-    curl_easy_setopt(curl, CURLOPT_URL, message);
-
-    curl_easy_setopt(curl, CURLOPT_TIMEOUT, TIMEOUT_SECONDS);
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "username=Evanok&password=evangelius");
-    curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen("username=Evanok&password=evangelius"));
-    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
-
-    res = curl_easy_perform(curl);
-
-    if(res != CURLE_OK)
-    {
-      if (chunk.memory)
-	free(chunk.memory);
-    }
-
-    if (chunk.memory)
-    {
-      PRINT_DEBUG ("[DEBUG] answer : |%s|\n", chunk.memory);
-    }
-
-    curl_easy_cleanup(curl);
+    T411_LOG (LOG_ERR, "Failed to initialiaze curl module");
+    return NULL;
   }
 
-  free (message);
+  T411_LOG (LOG_DEBUG, "url : |%s|\n", full_url);
+  T411_LOG (LOG_DEBUG, "message : |%s|\n", message);
+
+  curl_easy_setopt(curl, CURLOPT_URL, full_url);
+  curl_easy_setopt(curl, CURLOPT_ERRORBUFFER, buf_error);
+  curl_easy_setopt(curl, CURLOPT_TIMEOUT, TIMEOUT_SECONDS);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, message);
+  curl_easy_setopt(curl, CURLOPT_POSTFIELDSIZE, strlen(message));
+  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteMemoryCallback);
+  curl_easy_setopt(curl, CURLOPT_WRITEDATA, (void *)&chunk);
+
+  res = curl_easy_perform(curl);
+  if(res != CURLE_OK)
+  {
+    T411_LOG (LOG_ERR, "result != CURLE_OK");
+    T411_LOG (LOG_ERR, "Error : |%s|", buf_error);
+    return NULL;
+  }
+
+  if (!chunk.memory)
+  {
+    T411_LOG (LOG_ERR, "chunk memory is NULL !");
+    return NULL;
+  }
+
+  T411_LOG (LOG_DEBUG, "answer : |%s|\n", chunk.memory);
+  return chunk.memory;
+}
+
+
+int get_authentification (CURL *curl, const char* username, const char* password)
+{
+  char message[256];
+
+  sprintf (message, "username=%s&password=%s", username, password);
+  process_message (curl, "auth", message);
 
   return 0;
 }
