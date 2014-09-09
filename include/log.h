b80617d029f34b89c93e2bdcdf66e2504d072fe2
Arthur LAMBERT <lambertarthur22@gmail.com>
2014-09-09 16:51:43 +0200

    add a function of debug to dump all torrent info parsed by code

--- include/log.h
+++ include/log.h
@@ -22,5 +22,12 @@
 #define T411_LOG(a, ...) \
   do { if (DEBUG) fprintf(stderr, "\n[DEBUG] " __VA_ARGS__  ); else syslog(a, __VA_ARGS__);  } while (0)
 
+#if DEBUG
+#define dump_torrent(config) {						\
+    int i;for(i=0;i < config.nb_torrent;i++) T411_LOG(LOG_DEBUG, "list[%d] : %d %s %d %d \n", i, config.torrents[i].type, config.torrents[i].name, config.torrents[i].season, config.torrents[i].episode);\
+  }
+#else
+#define dump_torrent(config)
+#endif
 
 #endif /* !LOG_H_ */
